683495a4a5eccc322b4f5e8a89fe6a98637a42cf
satvik49 <sgf3f@mail.umkc.edu>
2016-07-13 14:42:39 -0500

    Commit

---
+++ source code/W2Vec_tweets/src/main/scala/SparkW2V.scala
@@ -0,0 +1,46 @@
+import java.io.File
+
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Mayanka on 21-Jun-16.
+  */
+object SparkW2V {
+  def main(args: Array[String]) {
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+
+    val sc = new SparkContext(sparkConf)
+
+    val input = sc.textFile("data/text9").map(line => line.split(" ").toSeq)
+
+    val modelFolder = new File("myModelPath")
+
+    if (modelFolder.exists()) {
+      val sameModel = Word2VecModel.load(sc, "myModelPath")
+      val synonyms = sameModel.findSynonyms("china", 40)
+
+      for ((synonym, cosineSimilarity) <- synonyms) {
+        println(s"$synonym $cosineSimilarity")
+      }
+    }
+    else {
+      val word2vec = new Word2Vec()
+
+      val model = word2vec.fit(input)
+
+      val synonyms = model.findSynonyms("China", 40)
+
+      for ((synonym, cosineSimilarity) <- synonyms) {
+        println(s"$synonym $cosineSimilarity")
+      }
+
+      // Save and load model
+      model.save(sc, "myModelPath")
+    }
+
+  }
+
+}
